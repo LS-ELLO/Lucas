5ef1667d4b8062589aa2c97e94ff96cd89d4dcbb
YM.Park-desktop <ympark4@ls-electric.com>
2021-05-31 11:08:07 +0900

    update: src folder __init__.py

--- mac/cookie_cutter_project_dir.sh
+++ mac/cookie_cutter_project_dir.sh
@@ -108,6 +108,28 @@ touch .gitkeep
 cd ../../src
 echo "Source code for use in this project." > README.md
 touch .gitkeep
+cat > __init__.py << EOF
+# 내부 폴더 *.py 파일을 다른 폴더에서 사용 가능하도록 해줌 
+
+import os, sys, inspect
+
+# https://gist.github.com/JungeAlexander/6ce0a5213f3af56d7369
+# 상위 폴더 연결하기 
+
+# 지금 폴더 & 부모 폴더 가져오기 
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir) 
+dirnames = os.listdir(parent_dir)   ## 부모 폴더내에 서브 폴더 가져 오기 
+dirnames= [i for i in dirnames if ("." not in i)]   ## 폴더 이름만 가져오기 (.py, .zip, .ds 형식 제외 )
+userpaths = [os.path.join(parent_dir, dirname) for dirname in dirnames] ## 서브 폴더의 완전한 주소 만들기 
+syspaths = sys.path ## 시스템 패스 정보 
+for userpath in userpaths:  ## User 패스 등록하기 
+    if userpath in syspaths:
+        pass
+    else:
+        sys.path.append(userpath)
+EOF
 mkdir dataread features models visualiztion
 cd ./dataread
 cat > __init__.py << EOF
